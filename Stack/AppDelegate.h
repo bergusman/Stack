4823e1c86cd68873d4a4dbcc786b3813ad5991b6
Vitaly Berg <bergusman@gmail.com>
2015-07-18 18:01:19 +0300

    Initial Commit

---
+++ Stack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stack
+//
+//  Created by Vitaly Berg on 18/07/15.
+//  Copyright (c) 2015 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stack
+//
+//  Created by Vitaly Berg on 18/07/15.
+//  Copyright (c) 2015 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
